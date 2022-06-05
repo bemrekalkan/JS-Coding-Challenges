2da9d27ab0b13de019d7bbf6d5a52f63b8478f25
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-05 11:46:49 +0300

    challenge is solved.

--- JS-CC-01_Reserve Words/app.js
+++ JS-CC-01_Reserve Words/app.js
@@ -1,1 +1,11 @@
-let reverseWords = () => {};
+//? created a function that take a string and reverse it.
+
+function reverseWords(str) {
+  let reverseWordArr = str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""));
+  return reverseWordArr.join(" ");
+}
+
+console.log(reverseWords("This is the first coding challenge."));
+//! output: 'sihT si eht tsrif gnidoc .egnellahc'
